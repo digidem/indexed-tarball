45df1e5913ad9b569122473e40408da977b59cf4
noffle <sww@eight45.net>
2018-08-22 14:46:05 -0700

    test(append): failing concurrent append test

--- test/append.js
+++ test/append.js
@@ -79,6 +79,52 @@ test('can append to an existing file', function (t) {
   })
 })
 
+test('two concurrent writes succeed as expected', function (t) {
+  tmp.dir({unsafeCleanup:true}, function (err, dir, cleanup) {
+    t.error(err, 'tmpdir setup')
+
+    var filepath = path.join(dir, 'file.tar')
+    var tarball = new Tarball(filepath)
+    var pending = 2
+
+    var data1 = 'greetings friend!'
+    tarball.append('hello.txt', fromString(data1), data1.length, function (err) {
+      t.error(err, 'append ok')
+      if (!--pending) check()
+    })
+
+    var data2 = '# beep boop'
+    tarball.append('beep.md', fromString(data2), data2.length, function (err) {
+      t.error(err, 'append ok')
+      if (!--pending) check()
+    })
+
+    function check () {
+      parseTarball(filepath, function (err, res) {
+        t.error(err, 'parsed tarball ok')
+
+        t.equals(res.length, 3, '3 entries')
+
+        t.equals(res[0].name, 'hello.txt', 'name matches')
+        t.equals(res[0].type, 'file', 'type matches')
+        t.equals(res[0].data.toString(), 'greetings friend!', 'content matches')
+
+        t.equals(res[1].name, 'beep.md', 'name matches')
+        t.equals(res[1].type, 'file', 'type matches')
+        t.equals(res[1].data.toString(), '# beep boop', 'content matches')
+
+        t.equals(res[2].name, '___index.json', 'contents match')
+        t.equals(res[2].type, 'file', 'type matches')
+        var index = JSON.parse(res[2].data.toString())
+        t.deepEquals(index, { 'hello.txt': { offset: 0 }, 'beep.md': { offset: 3072 } })
+
+        cleanup()
+        t.end()
+      })
+    }
+  })
+})
+
 function parseTarball (filepath, cb) {
   var res = []
   var error
