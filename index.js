6107a4d06a41fb3495811b8afc979678e338c74f
noffle <sww@eight45.net>
2018-08-21 18:23:46 -0700

    wip append impl

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 var fs = require('fs')
+var eos = require('end-of-stream')
+var tar = require('tar-stream')
 
 module.exports = IndexedTarball
 
 function IndexedTarball (filepath) {
   if (!(this instanceof IndexedTarball)) return new IndexedTarball(filepath)
 
-  this.fd = fs.openSync(filepath, 'w+'/*, 0666?*/)
+  this.filepath = filepath
 }
 
-IndexedTarball.prototype.append = function (filepath, readable, cb) {
-  process.nextTick(cb.bind(null, new Error('not implemented')))
+IndexedTarball.prototype.append = function (filepath, readable, size, cb) {
+  var pack = tar.pack()
+  var ps = pack.entry({ name: filepath, size: size }, function (err) {
+    if (err) return cb(err)
+    pack.finalize()
+    cb()
+  })
+  readable.pipe(ps)
+  pack.pipe(fs.createWriteStream(this.filepath))
 }
+
--- test/append.js
+++ test/append.js
@@ -1,26 +1,51 @@
 var Tarball = require('..')
-var from = require('from2')
+var collect = require('collect-stream')
+var fs = require('fs')
 var path = require('path')
 var tmp = require('tmp')
+var Readable = require('stream').Readable
+var tar = require('tar-stream')
 var test = require('tape')
 
 test('can append a file', function (t) {
   tmp.dir({unsafeCleanup:true}, function (err, dir, cleanup) {
-    var tarball = new Tarball(path.join(dir, 'file.tar'))
+    t.error(err, 'tmpdir setup')
 
-    tarball.append('hello.txt', fromString('greetings'), function (err) {
-      t.error(err)
-      cleanup()
-      t.end()
+    var tarball = new Tarball(path.join(dir, 'file.tar'))
+    tarball.append('hello.txt', fromString('greetings!'), 10, function (err) {
+      t.error(err, 'append ok')
+
+      // TODO: helper func to check that tarball matches expected data
+      var ex = tar.extract()
+      fs.createReadStream(path.join(dir, 'file.tar'))
+        .pipe(ex)
+      var entries = 0
+      ex.on('entry', function (header, stream, next) {
+        entries++
+        t.equals(entries, 1, 'one entry')
+        t.equals(header.name, 'hello.txt', 'contents match')
+        t.equals(header.type, 'file', 'type matches')
+        collect(stream, function (err, data) {
+          t.error(err)
+          t.equals(data, 'greetings!')
+          next()
+        })
+      })
+
+      ex.once('finish', function () {
+        cleanup()
+        t.end()
+      })
     })
   })
 })
 
-function fromString (string) {
-  return from(function(size, next) {
-    if (string.length <= 0) return next(null, null)
-    var chunk = string.slice(0, size)
-    string = string.slice(size)
-    next(null, chunk)
-  })
+function fromString (str) {
+  var data = new Readable()
+  data._read = function (size) {
+    if (str.length <= 0) return this.push(null)
+    var push = str.slice(0, size)
+    if (this.push(push)) str = str.slice(size)
+  }
+  return data
 }
