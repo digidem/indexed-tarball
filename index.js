3bdb368b61f09c655863f206b142d622d810d567
noffle <sww@eight45.net>
2018-08-21 17:50:08 -0700

    test: remove open test

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+var fs = require('fs')
+
 module.exports = IndexedTarball
 
 function IndexedTarball (filepath) {
   if (!(this instanceof IndexedTarball)) return new IndexedTarball(filepath)
+
+  this.fd = fs.openSync(filepath, 'w+'/*, 0666?*/)
 }
