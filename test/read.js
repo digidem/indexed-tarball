b9fecd5f139dc081cf5fbe3238ebae43dfbe6685
noffle <sww@eight45.net>
2018-08-23 16:14:42 -0700

    test: big read test

--- test/read.js
+++ test/read.js
@@ -62,3 +62,32 @@ test('can read an archive with two files', function (t) {
     })
   })
 })
+
+test('can read all files in an archive with many files', function (t) {
+  t.plan(301)
+
+  tmp.dir({unsafeCleanup:true}, function (err, dir, cleanup) {
+    t.error(err, 'tmpdir setup')
+
+    var filepath = path.join(dir, 'file.tar')
+    var tarball = new Tarball(filepath)
+
+    var n = 0
+    for (var i=0; i < 100; i++) {
+      n++
+      var data = 'this is message #' + i
+      tarball.append('hello_' + i + '.txt', fromString(data), data.length, function (err) {
+        t.error(err, 'append ok')
+      })
+    }
+
+    for (var i=0; i < n; i++) {
+      ;(function (x) {
+        collect(tarball.read('hello_' + i + '.txt'), function (err, data) {
+          t.error(err, 'read ok')
+          t.equals(data.toString(), 'this is message #' + x)
+        })
+      })(i)
+    }
+  })
+})
