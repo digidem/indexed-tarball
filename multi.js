5193123dcfe01f9f1832383b6ce8abd74a3df4d4
noffle <sww@eight45.net>
2018-08-27 16:21:17 -0700

    wip: multi-tarball implementation

--- multi.js
+++ multi.js
@@ -1,4 +1,5 @@
 var fs = require('fs')
+var path = require('path')
 var eos = require('end-of-stream')
 var tar = require('tar-stream')
 var RWLock = require('rwlock')
@@ -11,19 +12,54 @@ var tarUtil = require('./lib/tar')
 module.exports = MultiTarball
 
 function MultiTarball (filepath, opts) {
+  this.tarballs = []
+
+  this.loadLock = new RWLock()
+
+  this._setupTarballs()
 }
 
 MultiTarball.prototype.append = function (filepath, readable, size, cb) {
+  var self = this
+  this.loadLock.readLock(function (release) {
+  })
 }
 
 MultiTarball.prototype.list = function (cb) {
+  var self = this
+  this.loadLock.readLock(function (release) {
+  })
 }
 
 MultiTarball.prototype.read = function (filepath) {
+  var self = this
+  this.loadLock.readLock(function (release) {
+  })
 }
 
 MultiTarball.prototype.pop = function (cb) {
+  var self = this
+  this.loadLock.readLock(function (release) {
+  })
 }
 
-MultiTarball.prototype._writeNewIndex = function (newIndex, offset, cb) {
+MultiTarball.prototype._setupTarballs = function (cb) {
+  var self = this
+  cb = cb || noop
+
+  this.loadLock.writeLock(function (release) {
+    function done (err) {
+      release()
+      cb(err)
+    }
+
+    var dir = path.dirname(self.filepath)
+    fs.readdir(dir, function (err, contents) {
+      if (err) return done(err)
+      self.tarballs = contents.filter(function (name) { return name.startsWith(self.filepath) })
+      done()
+    })
+  })
 }
+
+function noop () {}
