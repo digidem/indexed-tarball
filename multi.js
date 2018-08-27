a8975a68097697b6144fbe38a43091d38aed0ce2
noffle <sww@eight45.net>
2018-08-27 16:59:50 -0700

    wip: multi-tarball append

--- multi.js
+++ multi.js
@@ -8,11 +8,16 @@ var readonly = require('read-only-stream')
 var fromBuffer = require('./lib/util').fromBuffer
 var cached = require('./lib/cached-value')
 var tarUtil = require('./lib/tar')
+var IndexedTarball = require('./single')
 
 module.exports = MultiTarball
 
 function MultiTarball (filepath, opts) {
+  opts = opts || {}
+
+  this.filepath = filepath
   this.tarballs = []
+  this.maxFileSize = opts.maxFileSize || (Math.pow(2, 32) - 1)
 
   this.loadLock = new RWLock()
 
@@ -22,6 +27,16 @@ function MultiTarball (filepath, opts) {
 MultiTarball.prototype.append = function (filepath, readable, size, cb) {
   var self = this
   this.loadLock.readLock(function (release) {
+    function done (err) {
+      release()
+      cb(err)
+    }
+
+    self._getLastTarball(function (err, tarball) {
+      if (err) return done(err)
+      // TODO: support case where tarball len + size > opts.maxFileSize
+      tarball.append(filepath, readable, size, done)
+    })
   })
 }
 
@@ -56,10 +71,65 @@ MultiTarball.prototype._setupTarballs = function (cb) {
     var dir = path.dirname(self.filepath)
     fs.readdir(dir, function (err, contents) {
       if (err) return done(err)
-      self.tarballs = contents.filter(function (name) { return name.startsWith(self.filepath) })
+      self.tarballs = contents
+        .filter(function (name) { return parseIndexFromFilename(name) !== null })
+        .map(function (name) { return new IndexedTarball(name) })
+        .sort(tarballCmp)
       done()
     })
   })
 }
 
+// Returns the final tarball in the set. A new one will be created if it doesn't exist.
+MultiTarball.prototype._getLastTarball = function (cb) {
+  var self = this
+  cb = cb || noop
+
+  this.loadLock.readLock(function (release) {
+    function done (err, res) {
+      release()
+      cb(err, res)
+    }
+
+    if (!self.tarballs.length) {
+      var tarball = new IndexedTarball(self.filepath)
+      self.tarballs.push(tarball)
+      done(null, tarball)
+    } else {
+      done(null, self.tarballs[self.tarballs.length - 1])
+    }
+  })
+}
+
 function noop () {}
+
+// Compares two IndexedTarball instances; sorting them so that the biggest indexed tarball filename comes last.
+function tarballCmp (a, b) {
+  var an = parseIndexFromFilename(a.filepath)
+  var bn = parseIndexFromFilename(b.filepath)
+  if (an === null || bn === null) return 0
+  if (an < bn) return -1
+  else if (an > bn) return 1
+  else return 0
+}
+
+// "foobar.tar.2"  => 2
+// "foobar.tar.3"  => 3
+// "foobar.tar"    => 0
+// "foobar.tar.hi" => null
+function parseIndexFromFilename (filename) {
+  if (/\.tar\.[0-9]+$/.test(filename)) {
+    try {
+      return parseInt(filename.match(/\.tar\.([0-9]+)$/)[1])
+    } catch (e) {
+      return null
+    }
+  } else {
+    return 0
+  }
+}
+
+// thanks https://stackoverflow.com/questions/2593637/how-to-escape-regular-expression-in-javascript#2593661
+function quoteRegex (str) {
+  return (str + '').replace(/[.?*+^$[\]\\(){}|-]/g, '\\$&')
+};
---
+++ test/append-multi.js
@@ -0,0 +1,125 @@
+var Tarball = require('..')
+var path = require('path')
+var tmp = require('tmp')
+var test = require('tape')
+var fromString = require('../lib/util').fromString
+var parseTarball = require('./util').parseTarball
+
+test('can append to a new file', function (t) {
+  tmp.dir({unsafeCleanup: true}, function (err, dir, cleanup) {
+    t.error(err, 'tmpdir setup')
+
+    var filepath = path.join(dir, 'file.tar')
+    var tarball = new Tarball(filepath, {multifile: true})
+    var data = 'greetings friend!'
+    tarball.append('hello.txt', fromString(data), data.length, function (err) {
+      t.error(err, 'append ok')
+
+      parseTarball(filepath, function (err, res) {
+        t.error(err, 'parsed tarball ok')
+
+        t.equals(res.length, 2, 'two entries')
+
+        t.equals(res[0].name, 'hello.txt', 'contents match')
+        t.equals(res[0].type, 'file', 'type matches')
+        t.equals(res[0].data.toString(), 'greetings friend!')
+
+        t.equals(res[1].name, '___index.json', 'contents match')
+        t.equals(res[1].type, 'file', 'type matches')
+        var index = JSON.parse(res[1].data.toString())
+        t.deepEquals(index, { 'hello.txt': { offset: 0, size: data.length } })
+
+        cleanup()
+        t.end()
+      })
+    })
+  })
+})
+
+return
+
+test('can append to an existing file', function (t) {
+  tmp.dir({unsafeCleanup: true}, function (err, dir, cleanup) {
+    t.error(err, 'tmpdir setup')
+
+    var filepath = path.join(dir, 'file.tar')
+    var tarball = new Tarball(filepath)
+    var data = 'greetings friend!'
+    tarball.append('hello.txt', fromString(data), data.length, function (err) {
+      t.error(err, 'append ok')
+
+      data = '# beep boop'
+      tarball.append('beep.md', fromString(data), data.length, function (err) {
+        t.error(err, 'append ok')
+
+        parseTarball(filepath, function (err, res) {
+          t.error(err, 'parsed tarball ok')
+
+          t.equals(res.length, 3, '3 entries')
+
+          t.equals(res[0].name, 'hello.txt', 'name matches')
+          t.equals(res[0].type, 'file', 'type matches')
+          t.equals(res[0].data.toString(), 'greetings friend!', 'content matches')
+
+          t.equals(res[1].name, 'beep.md', 'name matches')
+          t.equals(res[1].type, 'file', 'type matches')
+          t.equals(res[1].data.toString(), '# beep boop', 'content matches')
+
+          t.equals(res[2].name, '___index.json', 'contents match')
+          t.equals(res[2].type, 'file', 'type matches')
+          var index = JSON.parse(res[2].data.toString())
+          t.deepEquals(index, { 'hello.txt': { offset: 0, size: 17 }, 'beep.md': { offset: 1024, size: 11 } })
+
+          cleanup()
+          t.end()
+        })
+      })
+    })
+  })
+})
+
+test('two concurrent writes succeed as expected', function (t) {
+  tmp.dir({unsafeCleanup: true}, function (err, dir, cleanup) {
+    t.error(err, 'tmpdir setup')
+
+    var filepath = path.join(dir, 'file.tar')
+    var tarball = new Tarball(filepath)
+    var pending = 2
+
+    var data1 = 'greetings friend!'
+    tarball.append('hello.txt', fromString(data1), data1.length, function (err) {
+      t.error(err, 'append ok')
+      if (!--pending) check()
+    })
+
+    var data2 = '# beep boop'
+    tarball.append('beep.md', fromString(data2), data2.length, function (err) {
+      t.error(err, 'append ok')
+      if (!--pending) check()
+    })
+
+    function check () {
+      parseTarball(filepath, function (err, res) {
+        t.error(err, 'parsed tarball ok')
+
+        t.equals(res.length, 3, '3 entries')
+
+        t.equals(res[0].name, 'hello.txt', 'name matches')
+        t.equals(res[0].type, 'file', 'type matches')
+        t.equals(res[0].data.toString(), 'greetings friend!', 'content matches')
+
+        t.equals(res[1].name, 'beep.md', 'name matches')
+        t.equals(res[1].type, 'file', 'type matches')
+        t.equals(res[1].data.toString(), '# beep boop', 'content matches')
+
+        t.equals(res[2].name, '___index.json', 'contents match')
+        t.equals(res[2].type, 'file', 'type matches')
+        var index = JSON.parse(res[2].data.toString())
+        t.deepEquals(index, { 'hello.txt': { offset: 0, size: 17 }, 'beep.md': { offset: 1024, size: 11 } })
+
+        cleanup()
+        t.end()
+      })
+    }
+  })
+})
